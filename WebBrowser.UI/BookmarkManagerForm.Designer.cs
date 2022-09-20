6c826805a50324b9afe588fd325eaa511747af5d
Jack Odom <jeo0011@auburn.edu>
2022-09-20 17:05:23 -0500

    Added BookmarkManagerForm Form.

---
+++ WebBrowser.UI/BookmarkManagerForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace WebBrowser.UI
+{
+    partial class BookmarkManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BookmarkManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "BookmarkManagerForm";
+            this.Text = "BookmarkManagerForm";
+            this.Load += new System.EventHandler(this.BookmarkManagerForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
