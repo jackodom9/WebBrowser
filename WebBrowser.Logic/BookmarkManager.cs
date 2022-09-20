b65b1c329b1e476cd812893d2a93ee990bc34c2f
Jack Odom <jeo0011@auburn.edu>
2022-09-20 16:48:03 -0500

    Added static methods to BookmarkManager and HistoryManager to add and retrieve items in tables.

--- WebBrowser.Logic/BookmarkManager.cs
+++ WebBrowser.Logic/BookmarkManager.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebBroser.Data.BookmarksDataSetTableAdapters;
 
 namespace WebBrowser.Logic
 {
     public class BookmarkManager
     {
+        public static void AddItem(BookmarkItem item)
+        {
+            var adapter = new BookmarksTableAdapter();
+            adapter.Insert(item.URL, item.Title);
+        }
+
+        public static List<BookmarkItem> GetItems()
+        {
+            var adapter = new BookmarksTableAdapter();
+            var results = new List<BookmarkItem>();
+            var rows = adapter.GetData();
+            foreach (var row in rows)
+            {
+                var item = new BookmarkItem();
+                item.Title = row.Title;
+                item.URL = row.URL;
+                results.Add(item);
+            }
+            return results;
+        }
     }
 }
--- WebBrowser.Logic/HistoryManager.cs
+++ WebBrowser.Logic/HistoryManager.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebBroser.Data.HistoryDataSetTableAdapters;
 
 namespace WebBrowser.Logic
 {
     public class HistoryManager
     {
+        public static void AddItem(HistoryItem item)
+        {
+            var adapter = new HistoryTableAdapter();
+            adapter.Insert(item.Title, item.URL, item.Date);
+        }
+
+        public static List<HistoryItem> GetItems()
+        {
+            var adapter = new HistoryTableAdapter();
+            var results = new List<HistoryItem>();
+            var rows = adapter.GetData();
+
+            foreach (var row in rows)
+            {
+                var item = new HistoryItem();
+                item.Title = row.Title;
+                item.URL = row.URL;
+                item.Date = row.Date;
+                results.Add(item);
+            }
+            return results;
+        }
     }
 }
